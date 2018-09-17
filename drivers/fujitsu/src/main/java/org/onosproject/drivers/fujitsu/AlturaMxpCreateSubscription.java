15067c7fcebef930e1a48f0e4f35c90a86afbb42
mkleiner <kleiner.matias@gmail.com>
2018-09-17 13:43:24 -0300

    subscription

---
+++ drivers/fujitsu/src/main/java/org/onosproject/drivers/fujitsu/AlturaMxpCreateSubscription.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onosproject.drivers.fujitsu;
+
+        import org.onosproject.net.behaviour.MxpSubscription;
+        import org.onosproject.mastership.MastershipService;
+        import org.onosproject.net.DeviceId;
+        import org.onosproject.net.driver.AbstractHandlerBehaviour;
+        import org.onosproject.net.driver.DriverHandler;
+        import org.onosproject.netconf.NetconfController;
+        import org.onosproject.netconf.NetconfException;
+        import org.slf4j.Logger;
+
+        import static com.google.common.base.Preconditions.checkNotNull;
+        import static org.onosproject.drivers.fujitsu.FujitsuVoltXmlUtility.*;
+        import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Implementation to get all available data in vOLT
+ * through the Netconf protocol.
+ */
+public class AlturaMxpCreateSubscription extends AbstractHandlerBehaviour
+        implements MxpSubscription {
+
+    private final Logger log = getLogger(AlturaMxpCreateSubscription.class);
+
+
+    @Override
+    public String createSubscription() {
+        DriverHandler handler = handler();
+        NetconfController controller = handler.get(NetconfController.class);
+        MastershipService mastershipService = handler.get(MastershipService.class);
+        DeviceId ncDeviceId = handler.data().deviceId();
+        checkNotNull(controller, "Netconf controller is null");
+        String reply = null;
+
+        if (!mastershipService.isLocalMaster(ncDeviceId)) {
+            log.warn("Not master for {} Use {} to execute command",
+                    ncDeviceId,
+                    mastershipService.getMasterFor(ncDeviceId));
+            return null;
+        }
+
+        try {
+            StringBuilder   request = new StringBuilder("<create-subscription xmlns=\"urn:ietf:params:xml:ns:netconf:notification:1.0\"/>");
+
+            reply = controller
+                    .getDevicesMap()
+                    .get(ncDeviceId)
+                    .getSession()
+                    .doWrappedRpc(request.toString());
+        } catch (NetconfException e) {
+            log.error("Cannot communicate to device {} exception {}", ncDeviceId, e);
+        }
+        return reply;
+    }
+
+}
