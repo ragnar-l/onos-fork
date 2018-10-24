785b7e2399cc664e532342b8c1eb587fd913cbee
matias <kleiner.matias@gmail.com>
2018-10-24 14:17:26 -0300

    prueba de alcanzabilidad

--- providers/netconf/device/src/main/java/org/onosproject/provider/netconf/device/impl/NetconfDeviceProvider.java
+++ providers/netconf/device/src/main/java/org/onosproject/provider/netconf/device/impl/NetconfDeviceProvider.java
@@ -58,8 +58,12 @@ import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static org.onlab.util.Tools.groupedThreads;
 import static org.slf4j.LoggerFactory.getLogger;
 
-import org.onosproject.netconf.NetconfSession;
-import org.onosproject.net.driver.AbstractHandlerBehaviour;
+import java.io.InputStream;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 /**
  * Provider which uses an NETCONF controller to detect device.
@@ -296,28 +300,9 @@ public class NetconfDeviceProvider extends AbstractProvider
         //test connection to device opening a socket to it.
         log.debug("Testing reachability for {}:{}", ip, port);
 
-        NetconfController controller = checkNotNull(handler().get(NetconfController.class));
-        NetconfSession session = controller.getDevicesMap().get(handler().data().deviceId()).getSession();
-        StringBuilder request = new StringBuilder("<get>");
-        request.append("<filter type=\"subtree\">");
-        request.append("<mux-state xmlns=\"http://fulgor.com/ns/cli-mxp\">");
-        request.append("<device_manufacturer/>");
-        request.append("<device_swVersion/>");
-        request.append("<device_hwVersion/>");
-        request.append("<device_boardId/>");
-        request.append("</mux-state>");
-        request.append("</filter>");
-        request.append("</get>");
-        String version = null;
-
-        try {
-            version = session.doWrappedRpc(request.toString());
-        } catch (NetconfException e) {
-            throw new IllegalStateException(new NetconfException("Failed to retrieve version info.", e));
-        }
 
         log.info("PRUEBAAAA");
-        log.info(version);
+        log.info("");
 
         try (Socket socket = new Socket(ip, port)) {
             log.debug("rechability of {}, {}, {}", deviceId, socket.isConnected(), !socket.isClosed());
