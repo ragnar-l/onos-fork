562b0d7ac532a12141f755effa7d6f25937812eb
matias <kleiner.matias@gmail.com>
2018-11-02 20:20:47 -0300

    update linkdiscovery

--- drivers/fujitsu/src/main/java/org/onosproject/drivers/fujitsu/AlturaMxpDeviceDescription.java
+++ drivers/fujitsu/src/main/java/org/onosproject/drivers/fujitsu/AlturaMxpDeviceDescription.java
@@ -83,7 +83,7 @@ public class AlturaMxpDeviceDescription extends AbstractHandlerBehaviour
         devicecontroller.localStatus(deviceId);
 
         log.info("HOLAAAA");
-        log.info(devicecontroller.localStatus(deviceId).substring(11,11));
+        log.info(devicecontroller.localStatus(deviceId).substring(10,12));
 
         NetconfDevice ncDevice = controller.getDevicesMap().get(handler().data().deviceId());
         if (ncDevice == null) {
@@ -93,7 +93,7 @@ public class AlturaMxpDeviceDescription extends AbstractHandlerBehaviour
             return null;
         }
 
-        if (!(Integer.parseInt(devicecontroller.localStatus(deviceId).substring(9,11))>10)) {
+        if (!(Integer.parseInt(devicecontroller.localStatus(deviceId).substring(10,11))>10)) {
             log.info("NO ESTA ACTIVO");
             return null;
         }
