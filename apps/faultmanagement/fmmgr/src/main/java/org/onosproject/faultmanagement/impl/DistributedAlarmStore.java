00c80797a16a684ee59f56e6c474722e2c47c624
matias <kleiner.matias@gmail.com>
2018-11-13 21:22:31 -0300

    update onos

--- apps/faultmanagement/fmmgr/src/main/java/org/onosproject/faultmanagement/impl/DistributedAlarmStore.java
+++ apps/faultmanagement/fmmgr/src/main/java/org/onosproject/faultmanagement/impl/DistributedAlarmStore.java
@@ -118,9 +118,20 @@ public class DistributedAlarmStore
     public void createOrUpdateAlarm(Alarm alarm) {
         Alarm existing = alarmsMap.get(alarm.id());
         if (Objects.equals(existing, alarm)) {
-            log.debug("Received identical alarm, no operation needed on {}", alarm.id());
+            log.info("Received identical alarm, no operation needed on {}", alarm.id());
         } else {
-            alarms.put(alarm.id(), alarm);
+            if ( alarm.description().contains("[--]") && alarm.description().contains("mux-notify xmlns")) {
+                log.info("SI CONTIENE");
+                try {
+                    alarms.remove(alarm.id());
+                }
+                catch (Exception e){
+
+                }
+            }
+            else {
+                alarms.put(alarm.id(), alarm);
+            }
         }
     }
 
